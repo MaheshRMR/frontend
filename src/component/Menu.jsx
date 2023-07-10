4abf25670ee51c64f286a4c8e8c7b97c81ee6f5b
MaheshRMR <rameshandramesh11@gmail.com>
2023-07-10 19:28:07 +0530

    project

---
+++ src/component/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import'../style/Menu.css'
+
+function Menu() {
+  return (
+    <div className='menu'>Menu</div>
+  )
+}
+
+export default Menu
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../style/Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+      Vender Management
+        <ul>
+            {/* <li><Link  to="/home">Home</Link></li> */}
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
